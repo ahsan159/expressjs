7f2abd496ada8dd0691f47ca006ca95bb0fc05b8
ahsan159 <ahsan.05e159@student.uet.edu.pk>
2024-04-15 12:14:15 +0500

    using variable and query

--- app1/data.js
+++ app1/data.js
@@ -11,6 +11,9 @@ app.listen(8000, () => {
 
 // multiline response
 app.get("/", (req, res) => {
+  console.log(req);
+  console.log("original url:" + req.originalUrl);
+  console.log("params:" + req);
   res.write("This is line one");
   res.write(" this is line two");
   res.send(); // if you donot write this line in multiline site will keep waiting for end of response
@@ -18,10 +21,29 @@ app.get("/", (req, res) => {
 
 // send json object
 app.get("/temp", (req, res) => {
+  console.log(req.query);
+  // console.log(req);
+  console.log(req.query.unit);
+  console.log(req.query.city);
+  console.log(req.query.id);
+  let temp = 100;
+  switch (req.query.unit) {
+    case "C":
+      temp = temp * 1;
+      break;
+    case "F":
+      temp = temp * 1.8 + 32;
+      break;
+    case "K":
+      temp = temp + 273.15;
+      break;
+    default:
+      temp = temp * 1;
+  }
   res.send({
     id: 1,
-    name: "ahsan",
-    temp: 22.3,
+    name: req.query.city,
+    temp: temp,
     max: 24.3,
     min: 12.1,
     unit: "C",
@@ -30,5 +52,5 @@ app.get("/temp", (req, res) => {
 
 // this res.json function stringify any data as json like undefined or null values;
 app.get("/undefined", (req, res) => {
-    res.json(`this is ` + Number(0/0));
+  res.json(`this is ` + Number(0 / 0));
 });
